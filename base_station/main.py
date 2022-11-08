128da1d94fc53c1a7bdcdd8c76bd7a8645efddcf
danny525 <71981569+danny525@users.noreply.github.com>
2022-11-08 18:33:37 -0500

    extracts jpeg metadata and pulls the gps coordinates

--- base_station/main.py
+++ base_station/main.py
@@ -1,15 +1,23 @@
 # Outlining the high level models for now, we need to discuss further in detail how we want to design our application 
 
 from system_config import BaseStationConfigurations
-from logging import log_input_data
 from comms import get_input
 import base64
+from exif import Image
 import requests
 
 # Since we only want to get a PoC out of this, we should focus mainly on the fundamental functions, 
 # hence I think we should completely single-threaded-monolithic implementation
 
 ML_URL = ''
+
+from exif import Image
+
+def decimal_coords(coords, ref):
+    decimal_degrees = coords[0] + coords[1] / 60 + coords[2] / 3600
+    if ref == "S" or ref =='W' :
+        decimal_degrees = -decimal_degrees
+    return decimal_degrees
 
 if __name__ == "__main__":
     
@@ -20,15 +28,32 @@ if __name__ == "__main__":
     images = get_input()
     
     for image_path in images:
-        
+        print(image_path)
+
         with open(image_path, "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read())
-        # print(encoded_string)
+            # print(encoded_string)
         
-        # 2. Encode image into byte string (converted using blob)
-        # 3. Request ML API for prediction
-        r = requests.post(ML_URL, data={'image': encoded_string})
-        # return fields are TBD
+            # 2. Encode image into byte string (converted using blob)
+            # 3. Request ML API for prediction
+            # r = requests.post(ML_URL, data={'image': encoded_string})
+            # return fields are TBD
 
+        img = Image(image_path)
+        if img.has_exif:
+            try:
+                img.gps_longitude
+                coords = (decimal_coords(img.gps_latitude,
+                        img.gps_latitude_ref),
+                        decimal_coords(img.gps_longitude,
+                        img.gps_longitude_ref))
+                print({"imageTakenTime":img.datetime_original})
+                print(coords)
+            except AttributeError:
+                print ('No Coordinates')
+        else:
+            print('The Image has no EXIF information')
+            
         # 4. Wait for prediction
         # 5. Send alert if fire, and log the (image, time, gps, prediction)
+
