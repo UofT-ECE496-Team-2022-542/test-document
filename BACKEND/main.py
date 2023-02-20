14d99667e279a31dda7a9aa2b19c6442ebb20fe3
Mayukh Haldar <mayukhhaldar@gmail.com>
2023-02-20 18:58:12 -0500

    Adding code that contains the configuration with the drone

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,14 +1,16 @@
 # Outlining the high level models for now, we need to discuss further in detail how we want to design our application 
 
 from system_config import BaseStationConfigurations
+from drone import DroneController
 from comms import get_input
 import base64
 #from exif import Image
-import requests
 import json
 import logger
 import datetime
 import os
+
+import cv2
 
 ##############################
 ### IMAGE CLASSIFIER SETUP ###
@@ -21,11 +23,9 @@ model = ImageClassifier()
 model.load_state_dict(torch.load('ResNet_Models/epoch_'+str(4)+'.pth', map_location=torch.device('cpu')))
 model.eval()
 
-#ML_API_url = 'http://127.0.0.1:8000/predict/'
 
 ##############################
 
-#from exif import Image
 
 def decimal_coords(coords, ref):
     decimal_degrees = coords[0] + coords[1] / 60 + coords[2] / 3600
@@ -33,6 +33,57 @@ def decimal_coords(coords, ref):
         decimal_degrees = -decimal_degrees
     return decimal_degrees
 
+def image_capture(drone):
+    global count
+    img = drone.drone.get_frame_read().frame
+    #cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+    im_pil = Image.fromarray(img)
+    r = forward_pass(model=model, image=im_pil)
+    
+    print("Model Result: {}".format(r))
+    
+    name = 'saved_images/image'+ r + str(count) + '.jpg'
+    print(name)
+
+    status = cv2.imwrite(name,img)
+    count += 1
+    print(status)
+    
+def surveillance(my_drone):
+    rotate = 0
+    offset1 = 1
+    offset2 = 2
+    while rotate < 8:
+        image_capture(my_drone)
+        if rotate % 2 == 0:  
+            my_drone.drone.rotate_clockwise(45+offset1)
+        else:
+            my_drone.drone.rotate_clockwise(45+offset2)
+        rotate += 1
+    
+def execute_flight_path(my_drone):
+    image_capture(my_drone)
+    my_drone.drone.takeoff()
+    surveillance(my_drone)
+    #go_xyz_speed(x, y, z, speed)
+    my_drone.drone.go_xyz_speed(0,0,250,40)
+    surveillance(my_drone)
+    #up and right by 200 cm
+    my_drone.drone.go_xyz_speed(200,200,0,40)
+    surveillance(my_drone)
+    my_drone.drone.go_xyz_speed(200,-200,0,40)
+    my_drone.drone.go_xyz_speed(-200,-200,0,40)
+    my_drone.drone.go_xyz_speed(-200,200,0,40)
+    my_drone.drone.go_xyz_speed(0,0,-250,20)
+    my_drone.drone.land()
+    
+def execute_flight_path(my_drone):
+    while True:
+        image_capture(my_drone)
+   
+    
 #First, reset database and logs
 files_to_reset = ['databases/predicitons.db',
                     'logs/prediciton_log.csv']
@@ -42,76 +93,46 @@ for file in files_to_reset:
     # f = open(file, "x")
 
 if __name__ == "__main__":
+    count = 0
     # initialize the system configurations and store it
     config = BaseStationConfigurations()
+    my_drone = DroneController()
     
-    # gets list of image paths
-    images = get_input()
+    my_drone.drone.streamon()
+    my_drone.pre_flight_checks()
     
-    #### TESTING BLOCK START ####
+    execute_flight_path(my_drone)
+    
+    my_drone.drone.streamoff()
+    
+        #### TESTING BLOCK START ####
     print("\n----------------------------------------")
     print("RUNNING TESTS ... ... ...")
-    num_correct = 0
     #### TESTING BLOCK END ####
     
-    for image_path in images:
+    
+    
+    """ while True:
         with open(image_path, "rb") as image_file:
             img_blob = base64.b64encode(image_file.read())
             encoded_img = img_blob.decode('utf-8')
             # print(encoded_string)
         
-        # 2. Encode image into byte string (converted using blob)
-        # 3. Request ML API for prediction
-        ## response = requests.post(ML_API_url, data=json.dumps({"image": encoded_img}), headers = {"content-type": "application/json"})
-        ## r = response.text
-        r = forward_pass(model=model, image=Image.open(image_path))
+        img = me.get_frame_read().frame
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
         
-        #### TESTING BLOCK START ####
-        expected_r = 'ERROR(BAD FILENAME)'
-        try:
-            if os.path.basename(image_path)[:2] == 'F_':
-                expected_r = 'FIRE'
-            elif os.path.basename(image_path)[:3] == 'NF_':
-                expected_r = 'NO_FIRE'
-        except Exception as e:
-            print(e)
-        print("\nTEST CASE: {}".format(image_path))
-        print("Expected Result: {}".format(expected_r))
+        im_pil = Image.fromarray(img)
+        r = forward_pass(model=model, image=im_pil)
+        
         print("Actual Result: {}".format(r))
-        if expected_r == r:
-            num_correct+=1
-        #### TESTING CODE BLOCK END ####
         
-        # return fields are TBD
-
-        # img = Image(image_path)
-        # if img.has_exif:
-        #     try:
-        #         img.gps_longitude
-        #         coords = (decimal_coords(img.gps_latitude,
-        #                 img.gps_latitude_ref),
-        #                 decimal_coords(img.gps_longitude,
-        #                 img.gps_longitude_ref))
-        #         print({"imageTakenTime":img.datetime_original})
-        #         print(coords)
-        #     except AttributeError:
-        #         print ('No Coordinates')
-        # else:
-        #     print('The Image has no EXIF information')
-            
-        # 4. Wait for prediction
-        # 5. Send alert if fire, and log the (image, time, gps, prediction)
+       
         currentDateTime = datetime.datetime.now()
-        logger.add_prediction(config.log_id, currentDateTime, 12.5, -15.541, img_blob, r, config.db_name)
-        config.log_id += 1
+        #logger.add_prediction(config.log_id, currentDateTime, 12.5, -15.541, img_blob, r, config.db_name)
+        config.log_id += 1 """
     
-    #### TESTING BLOCK START ####
-    print("\n ... ... ... TEST COMPLETE")
-    print("{}/{} ({:.2f}%) predictions correct.".format(
-        num_correct, len(images), 100*num_correct/len(images)
-    ))
-    print("----------------------------------------\n")
-    #### TESTING BLOCK END ####
+   
     
     logger.print_db_log(config.db_name)
     logger.close_databases(config.db_name)
--- BACKEND/system_config.py
+++ BACKEND/system_config.py
@@ -1,16 +1,14 @@
 from logger import initialize_database
+from djitellopy import tello
 
 class BaseStationConfigurations:
     db_name = "databases/predictions.db"
     log_id = 0
     
     def __init__(self) -> None:
-        
-        #initialize connection with drone
-        #self.drone_conn = self.initialize_drone_connection()
-        
+                
         #initialize connection with database
         initialize_database(self.db_name)
         
-    def initialize_drone_connection():
-        return None #Not Implmented
+ 
+        
